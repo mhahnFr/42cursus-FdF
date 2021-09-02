91627dc1d8b187463627b470b42568db01c2fdd4
mhahn <mhahn@student.42heilbronn.de>
2021-09-02 23:44:35 +0200

    Added commandline interface directory

---
+++ CLI/cli.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+#include ".libft/libft.h"
+#include "cli.h"
+
+
+static void print_help_exit(const char *app_name)
+{
+	
+}
+
+void	cli(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		
+	}
+}
