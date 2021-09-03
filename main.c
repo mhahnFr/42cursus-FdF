f653943ee68318a0c03e6c28f64cdd5825cafb21
mhahn <mhahn@student.42heilbronn.de>
2021-09-03 23:58:34 +0200

    Added command line interface to main

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "./delegate/app_delegate.h"
 #include "./view/init.h"
 #include "./model/generate.h"
+#include "./CLI/cli.h"
 
 int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*model;
 
+	cli(argc, argv);
 	model = parse_arguments(argc, argv);
 	mlx = init_mlx();
 	return (onApplicationFinishedLaunching(model, mlx));
