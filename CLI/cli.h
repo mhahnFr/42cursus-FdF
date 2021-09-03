16b037d5401cf30bf0c9f5c587eda66dc41e6be2
mhahn <mhahn@student.42heilbronn.de>
2021-09-03 23:59:05 +0200

    Added some documentation

--- CLI/cli.h
+++ CLI/cli.h
@@ -3,4 +3,15 @@
 
 void	cli(int argc, char **argv);
 
+/*
+ * Prints a small help screen and exits the application. Takes as parameter the
+ * name of the executable in order to display it properly to the user.
+ */
+void	print_help_exit(const char *app_name);
+
+/*
+ * Parses the given arguments.
+ */
+void	cli(int argc, char **argv);
+
 #endif
