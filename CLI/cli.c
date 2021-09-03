39dc250dea86b0c4539b10adebe46801c86dbd08
mhahn <mhahn@student.42heilbronn.de>
2021-09-03 23:58:11 +0200

    Added usage printing

--- CLI/cli.c
+++ CLI/cli.c
@@ -1,18 +1,18 @@
-#include <unistd.h>
+#include <stdlib.h>
 
-#include ".libft/libft.h"
+#include "libft.h"
 #include "cli.h"
 
-
-static void print_help_exit(const char *app_name)
+void	print_help_exit(const char *app_name)
 {
-	
+	ft_putstr_fd("Usage: ", 1);
+	ft_putstr_fd((char *) app_name, 1);
+	ft_putstr_fd(" <file> \n", 1);
+	exit(0);
 }
 
 void	cli(int argc, char **argv)
 {
 	if (argc == 1)
-	{
-		
-	}
+		print_help_exit(argv[0]);
 }
