33fb6f3f0c1662516572e22f0d366507ad32fefd
mhahn <mhahn@student.42heilbronn.de>
2021-09-20 17:17:14 +0200

    Replaced libft functions by ft_printf

--- CLI/cli.c
+++ CLI/cli.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 
-#include "libft.h"
 #include "cli.h"
+#include "ft_printf.h"
 
 void	print_help_exit(const char *app_name)
 {
-	ft_putstr_fd("Usage: ", 1);
-	ft_putstr_fd((char *) app_name, 1);
-	ft_putstr_fd(" <file> \n", 1);
+	ft_printf("Usage: %s <file> \n", app_name);
 	exit(0);
 }
 
 void	cli(int argc, char **argv)
 {
 	if (argc == 1)
+		print_help_exit(argv[0]);
+	else if (argc > 2)
 		print_help_exit(argv[0]);
 }
