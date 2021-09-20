2bf21a04897eb2154aed29ccb375d8e60424d3c4
mhahn <mhahn@student.42heilbronn.de>
2021-09-20 17:18:22 +0200

    Removed doubled function definition

--- CLI/cli.h
+++ CLI/cli.h
@@ -1,8 +1,6 @@
 #ifndef CLI_H
 # define CLI_H
 
-void	cli(int argc, char **argv);
-
 /*
  * Prints a small help screen and exits the application. Takes as parameter the
  * name of the executable in order to display it properly to the user.
