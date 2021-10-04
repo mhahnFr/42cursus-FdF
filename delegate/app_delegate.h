a43115bc19feda07a69b815300d74c448d7e4f10
mhahn <mhahn@student.42heilbronn.de>
2021-10-04 20:43:53 +0200

    Added documentation

--- delegate/app_delegate.h
+++ delegate/app_delegate.h
@@ -13,7 +13,7 @@
 int		onApplicationFinishedLaunching(t_model3D *model, void *view);
 
 /*
- * Creates and returns a new window with the given parameters.
+ * Creates and returns a new window with the given size and the given title.
  */
 t_window	*delegate_get_new_window(
 				void *mlx_ptr,
@@ -22,15 +22,21 @@ t_window	*delegate_get_new_window(
 				char *title);
 
 /*
- * Delegates the event 
+ * Delegates the event of a pressed key to the window on which it happened.
+ * Tkaes as arguments the code of the pressed key and a pointer to the affected
+ * window. Returns always zero, as the return value is useless according to the
+ * documentation of the MiniLibX library.
  */
 int			delegate_key_touched(int key, void *window);
 
 /*
+ * Handles the key events for the main window. Takes as parameter the
+ * informations of the event.
  */
 void		delegate_main_window_key_touched(t_key_event *event);
 
 /*
+ * Closes the application. Removes all used memory. (WIP)
  */
 void		delegate_exit(void);
 
