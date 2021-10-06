aa68c24faba557d8094bda9ed42c4558bbb28a64
mhahn <mhahn@student.42heilbronn.de>
2021-10-06 02:10:54 +0200

    Added documentation

--- delegate/renderer.h
+++ delegate/renderer.h
@@ -1,12 +1,22 @@
 #ifndef RENDERER_H
 # define RENDERER_H
 
+/*
+ * Represents a renderer.
+ */
 typedef struct s_renderer
 {
 }	t_renderer;
 
+/*
+ * Creates a new renderer. Returns the newly allocated renderer, or null, if
+ * the allocation failed.
+ */
 t_renderer	*renderer_new(void);
 
+/*
+ * Deletes the given renderer. Does nothing if no object is given.
+ */
 void		renderer_delete(t_renderer *this);
 
 #endif
