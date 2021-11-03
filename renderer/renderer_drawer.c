c6454176936321dbe250db8ab37f75a235e7c7ab
mhahn <mhahn@student.42heilbronn.de>
2021-11-04 00:00:48 +0100

    Added free after use

--- renderer/renderer_drawer.c
+++ renderer/renderer_drawer.c
@@ -8,17 +8,23 @@ void	renderer_draw(t_delegate *this)
 	t_vertex3D	*v_tmp;
 	t_vertex3D	*prev;
 	t_matrix	*m_tmp;
+	t_matrix	*m_tmp2;
 	size_t		i;
 
 	prev = NULL;
 	i = 0;
 	while (i < this->model->vertex_count)
 	{
-		m_tmp = matrix_new_multiply(this->renderer->mvp, vertex3D_cast_matrix(
-					this->model->vertices[i]));
+		m_tmp2 = vertex3D_cast_matrix(this->model->vertices[i]);
+		m_tmp = matrix_new_multiply(this->renderer->mvp, m_tmp2);
+		matrix_delete(m_tmp2);
 		v_tmp = matrix_cast_vertex3D(m_tmp);
+		matrix_delete(m_tmp);
 		if (prev != NULL)
+		{
 			renderer_draw_line(prev, v_tmp, this);
+			vertex3D_delete(prev);
+		}
 		prev = v_tmp;
 		i++;
 	}
@@ -31,5 +37,4 @@ void	renderer_draw_line(
 {
 	first = second;
 	this = NULL;
-	// TODO rendern!!!
 }
