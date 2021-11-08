8d567c8c51cb04689a6dd0e3bd8cd5978e063362
mhahn <mhahn@student.42heilbronn.de>
2021-11-08 18:19:57 +0100

    Switched to native matrix and vertex multiplication

--- renderer/renderer_drawer.c
+++ renderer/renderer_drawer.c
@@ -9,19 +9,13 @@ void	renderer_draw(t_delegate *this)
 {
 	t_vertex3D	v_tmp;
 	t_vertex3D	*prev;
-	t_matrix	*m_tmp;
-	t_matrix	*m_tmp2;
 	size_t		i;
 
 	prev = NULL;
 	i = 0;
 	while (i < this->model->vertex_count)
 	{
-		m_tmp2 = vertex3D_cast_matrix(this->model->vertices[i]);
-		m_tmp = matrix_new_multiply(this->renderer->mvp, m_tmp2);
-		matrix_delete(m_tmp2);
-		matrix_cast_vertex3D(m_tmp, &v_tmp);
-		matrix_delete(m_tmp);
+		matrix_multiply_vertex3D(&v_tmp, this->renderer->mvp, this->model->vertices[i]);
 		if (prev != NULL)
 			renderer_draw_line(prev, &v_tmp, this->renderer->buffer);
 		prev = &v_tmp;
