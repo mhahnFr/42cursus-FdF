cb9294b93ac3d433829bf100ce5ae68008ff928f
mhahn <mhahn@student.42heilbronn.de>
2021-11-09 00:25:07 +0100

    Rewrote vector generation, still not working!

--- renderer/renderer_generate_vectors.c
+++ renderer/renderer_generate_vectors.c
@@ -8,38 +8,38 @@ void	renderer_generate_vectors(t_renderer_camera *cam)
 	cam->height = renderer_generate_up(cam);
 }
 
+#include <stdio.h>
 t_vector	*renderer_generate_depth(t_renderer_camera *cam)
 {
 	t_vector	pos;
 	t_vector	v_p;
 	t_vector	tmp;
+	double		norm;
 
 	vector_create(&pos, cam->pos->x, cam->pos->y, cam->pos->z);
 	vector_create(&v_p, cam->view_point->x, cam->view_point->y,
 		cam->view_point->z);
 	vector_substract(&tmp, &v_p, &pos);
-	return (vector_new_division(&tmp, vector_length(&tmp)));
+	printf("%f\n", vector_length(&tmp));
+	norm = 1 / vector_length(&tmp);
+	return (vector_new_multiply_digit(&tmp, norm));
 }
 
 t_vector	*renderer_generate_right(t_renderer_camera *cam)
 {
-	t_vector	tmp;
-	t_vector	w_u;
-
-	vector_create(&w_u, 0, 1, 0);
-	vector_multiply(&tmp, cam->depth, &w_u);
-	return (vector_new_division(&tmp, vector_length(&tmp)));
+	return (vector_new_multiply(cam->depth, cam->height));
 }
 
 t_vector	*renderer_generate_up(t_renderer_camera *cam)
 {
+	t_vector	w_u;
 	t_vector	tmp;
 	t_vector	tmp2;
-	t_vector	u_w;
+	float		norm;
 
-	vector_create(&u_w, 0, 1, 0);
-	vector_multiply(&tmp, cam->depth, &u_w);
-	vector_multiply(&tmp2, &tmp, cam->depth);
-	vector_substract(&tmp, &u_w, &tmp2);
-	return (vector_new_division(&tmp, vector_length(&tmp)));
+	vector_create(&w_u, 1, 1, 1);
+	vector_multiply_digit(&tmp, cam->depth, vector_scalar_product(cam->depth, &w_u));
+	vector_substract(&tmp2, &w_u, &tmp);
+	norm = 1 / vector_length(&tmp2);
+	return (vector_new_multiply_digit(&tmp2, norm));
 }
