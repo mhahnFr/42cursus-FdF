91c3800807c3ecb4402be791ed98505fecb85e24
mhahn <mhahn@student.42heilbronn.de>
2021-11-12 22:17:56 +0100

    Fixed crash when using unsafe size

--- utils/arraylist_size.c
+++ utils/arraylist_size.c
@@ -15,7 +15,7 @@ size_t	arraylist_size(t_arraylist *this)
 
 size_t	arraylist_size_unsafe(t_arraylist *this)
 {
-	while (this != NULL)
+	while (this->next != NULL)
 		this = this->next;
 	return (this->index + 1);
 }
