924630fea8d1311910b836f47f871537ed4f847f
mhahn <mhahn@student.42heilbronn.de>
2021-11-22 19:27:13 +0100

    Norm fixes

--- model/generate.h
+++ model/generate.h
@@ -13,20 +13,20 @@ typedef struct s_vertex3D	t_vertex3D;
  * based on this data. Takes the arguments and their count as parameters.
  * Returns a pointer to the model that has been created.
  */
-t_model3D	*generate_parse_arguments(t_cli *cli_obj);
+t_model3D		*generate_parse_arguments(t_cli *cli_obj);
 
 /*
  * Reads the file and splits it by its spaces. Returns an arraylist object
  * consisting of all lines read from the given file, splitted by the space.
  */
-t_arraylist	*generate_read_file(char *file_name);
+t_arraylist		*generate_read_file(char *file_name);
 
 /*
  * Deletes the given char array. The last element of the given array has to be
  * a null pointer, as it serves as indicator of the end of the array. Does
  * nothing if no rray is given.
  */
-void		generate_del_chr_ar(char **array);
+void			generate_del_chr_ar(char **array);
 
 /*
  * Converts the given arraylist containing the raw vertex data read from the
@@ -36,7 +36,7 @@ void		generate_del_chr_ar(char **array);
  *
  * Hint: probably use an array, as that won't change!
  */
-t_arraylist	*generate_convert_vertices(t_arraylist *raw_vertices);
+t_arraylist		*generate_convert_vertices(t_arraylist *raw_vertices);
 
 /*
  * Converts all arraylists inside of the given list to arrays and stores them
@@ -44,14 +44,14 @@ t_arraylist	*generate_convert_vertices(t_arraylist *raw_vertices);
  * returned. Uses the unsafe arraylist functions. Returns either the newly
  * allocated array or null if either the allocation failed or no list is given.
  */
-size_t		*generate_convert_and_count_contents(t_arraylist *list);
+size_t			*generate_convert_and_count_contents(t_arraylist *list);
 
 /*
  * Allocates a new vertex with the given data. Returns either the newly
  * allocated vertex or null if either the allocation failed or the string is
  * missing.
  */
-t_vertex3D	*generate_vertex(size_t x, size_t y, char *raw_z);
+t_vertex3D		*generate_vertex(size_t x, size_t y, char *raw_z);
 
 /*
  * Converts the given string to an unsigned int. The string has to contain a
@@ -64,16 +64,18 @@ unsigned int	generate_color(char *string);
  * Clears the two given lists with the given remove functions. Displays an
  * error message on the the standard error stream.
  */
-t_arraylist	*generate_error_clear_double(
-				t_arraylist **list,
-				t_arraylist_remover r,
-				t_arraylist **second,
-				t_arraylist_remover r_second);
+t_arraylist		*generate_error_clear_double(
+					t_arraylist **list,
+					t_arraylist_remover r,
+					t_arraylist **second,
+					t_arraylist_remover r_second);
 
 /*
  * Clears the given list using the given remove function. Displays an error
  * message on the standard error stream.
  */
-t_arraylist	*generate_error_clear(t_arraylist **list, t_arraylist_remover r);
+t_arraylist		*generate_error_clear(
+					t_arraylist **list,
+					t_arraylist_remover r);
 
 #endif
